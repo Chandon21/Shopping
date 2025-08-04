afb1dd98d056b02b4acf2fce3dc46cea2dab8cfe
Chandon21 <132583061+Chandon21@users.noreply.github.com>
2025-08-04 21:15:11 +0600

    Migrations

---
+++ 202501211845471_initdb.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initdb : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ShoppingLists", "Category", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ShoppingLists", "Category");
+        }
+    }
+}
