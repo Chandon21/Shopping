9e03fcd42679e00fd8f6f4a63d939c9ddcfcf7b1
Chandon21 <132583061+Chandon21@users.noreply.github.com>
2025-08-04 21:15:37 +0600

    Properties

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BLL")]
+[assembly: AssemblyTitle("DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BLL")]
+[assembly: AssemblyProduct("DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6cca1404-723d-4cbd-af28-e3c95aa2dc8a")]
+[assembly: Guid("bfd8310c-e3fe-43f1-abaf-d437d012c3cf")]
 
 // Version information for an assembly consists of the following four values:
 //
