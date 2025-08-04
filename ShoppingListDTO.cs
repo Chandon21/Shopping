9ca2d015f663cb5b768e005652c616027f922aa1
Chandon21 <132583061+Chandon21@users.noreply.github.com>
2025-08-04 21:11:22 +0600

    DTOs

---
+++ ShoppingListDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class ShoppingListDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } 
+        public List<ShopDTO> Items { get; set; }
+    }
+}
